eee3498ea53593f8b0486664217b33f2b288571c
vttinn_ <tapiajaram@gmail.com>
2026-09-04 12:17:35 -0400

    Update settings.py

--- sistema_ventas/settings.py
+++ sistema_ventas/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Registramos Las Aplicaciones Aquí
     'tienda',
     'crispy_forms',
     'crispy_bootstrap5',
